1b364e1a635c35f0a1ba47cef73b4313d17754f6
StrangerSparky <strangersparky009@gmail.com>
2026-06-26 20:10:06 +0530

    Re-enable auto-update checker, use main branch for version.txt

--- Telegram/SourceFiles/ayu/ayu_update_checker.cpp
+++ Telegram/SourceFiles/ayu/ayu_update_checker.cpp
@@ -11,7 +11,7 @@ namespace {
 
 constexpr auto kRepoOwner   = "StrangerSparky";
 constexpr auto kRepoName    = "SandyGram";
-constexpr auto kVersionFileBranch = "dev";
+constexpr auto kVersionFileBranch = "main";
 
 int g_latestVersion = 0;
 bool g_updateAvailable = false;
